66fd549e1eabd2ed3e75281064dbc2486d8fbd1f
Anirudhrarao <raorudhra16@gmail.com>
2024-09-03 06:03:17 +0530

    Array completed till slicing

--- 008_array.go
+++ 008_array.go
@@ -10,7 +10,9 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Length is fixed
 /*
@@ -42,10 +44,37 @@ func main() {
 */
 
 // Change Element of an Array
-
+/*
 func main() {
 	prices := [3]int{10, 20, 30}
 	fmt.Println("This is Original one: ", prices)
 	prices[2] = 50
 	fmt.Println("This is updated one: ", prices)
+}
+*/
+
+// Array Initialization
+/*
+func main() {
+	arr1 := [5]int{}     // Not initialize
+	arr2 := [5]int{1, 2} // Partially initialize
+	arr3 := [2]int{1, 2} // Full initialize
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+}
+*/
+
+// Initialize Only Specific Elements
+/*
+func main() {
+	arr1 := [4]int{1: 10, 3: 11}
+	fmt.Println(arr1)
+}
+*/
+
+// Length of Array
+func main() {
+	prices := [3]int{10, 20, 30}
+	fmt.Println(len(prices))
 }
